19f47dd03c6db6554676fab4d68efedf0f8b93f0
Artur Senko <asenko@apple.com>
2019-01-01 07:02:44 -0800

    Updates

---
+++ precache-manifest.a330a4faea55001e0f341576fafa8c90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "3b85a5cdb1fc43315f7d",
+    "url": "/react-gh-pages/static/js/main.3b85a5cd.chunk.js"
+  },
+  {
+    "revision": "2df9c0b7750d1d5d3646",
+    "url": "/react-gh-pages/static/js/1.2df9c0b7.chunk.js"
+  },
+  {
+    "revision": "2df9c0b7750d1d5d3646",
+    "url": "/react-gh-pages/static/css/1.1fc65526.chunk.css"
+  },
+  {
+    "revision": "e18c1e7f0d34710e71ff5cd55e0443a7",
+    "url": "/react-gh-pages/index.html"
+  }
+];
